601068aa66c4b5169ed43769859913eee720f169
Maicon Lipa <maiconaruquipa@gmail.com>
2025-04-03 15:50:32 -0300

    finalizando o projeto BASE completo para realizar novas implementações futuras para API

--- ProjetoBasicoApi/WebApiDotNet/Infrastructure/Data/ApplicationDbContext.cs
+++ ProjetoBasicoApi/WebApiDotNet/Infrastructure/Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ namespace WebApiDotNet.Infrastructure.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
